8be30ffaa7ed3563543f4cfbf8f83535f4e5abfc
yourGitHubUsername <yourEmail@gmail.com>
2026-04-18 12:52:38 +0530

    Added UC11 files

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===============================");
+        System.out.println("     PALINDROME CHECKER APP");
+        System.out.println("     Version: 1.0");
+        System.out.println("===============================");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        // Object creation (OOP)
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        if(result)
+            System.out.println(input + " is a Palindrome");
+        else
+            System.out.println(input + " is NOT a Palindrome");
+
+        sc.close();
+    }
+}
